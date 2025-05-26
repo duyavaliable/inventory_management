f9d6e0a81ffab59f123f93bd8241e6127bceef46
duyavaliable <noduy68@gmail.com>
2025-05-26 22:47:21 +0700

    first commit

---
+++ inventory/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class InventoryItem(models.Model):
+	name = models.CharField(max_length=200)
+	sku = models.CharField(max_length=50, unique=True, verbose_name="Mã sản phẩm")
+	quantity = models.IntegerField()
+	selling_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Giá bán") 
+	category = models.ForeignKey('Category', on_delete=models.SET_NULL, blank=True, null=True)
+	date_created = models.DateTimeField(auto_now_add=True)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
+
+class Category(models.Model):
+	name = models.CharField(max_length=200)
+
+	class Meta:
+		verbose_name_plural = 'categories'
+
+	def __str__(self):
+		return self.name
